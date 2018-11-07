983cea3e3b8cf32107808327823aed0a56cfb95a
scriptkitz <scriptkitz@gmail.com>
2018-11-07 14:23:42 +0800

    ByteArray

--- welcome/weiqi/weiqicmd_views.py
+++ welcome/weiqi/weiqicmd_views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 
-from . import ByteArray
+from ByteArray import ByteArray
 
 # Create your views here.
 
