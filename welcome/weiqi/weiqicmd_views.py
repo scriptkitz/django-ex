fd160f91cadbe7c2eb434103152f577b614d0c3e
scriptkitz <scriptkitz@gmail.com>
2018-11-07 16:44:36 +0800

    cmd201

--- welcome/weiqi/weiqicmd_views.py
+++ welcome/weiqi/weiqicmd_views.py
@@ -1,21 +1,23 @@
 import os
 import struct
+import xml.etree.ElementTree as ET
+
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 
 from .ByteArray import ByteArray
 
+
+module_dir = os.path.dirname(__file__)  # get current directory
 # Create your views here.
 
-def _cmd10(_bytes):
-    ba = ByteArray(_bytes)
+def _cmd_10(ba):
     name = ba.readUTF()
     print("name:%s"%name)
     newba = ByteArray()
     newba.writeShort(10)
     if name == "配置信息":
-        module_dir = os.path.dirname(__file__)  # get current directory
         file_path = os.path.join(module_dir, 'weiqi_conf.xml')
         fs = open(file_path,'rb')
         data = fs.read()
@@ -24,15 +26,28 @@ def _cmd10(_bytes):
         
     return newba.bytes
 
+def _cmd_201(ba):
+    device = ba.readInt()
+    imei = ba.readUTF()
+    lesson = ba.readByte()
+    print("-----")
+    print(type(lesson))
+    print(lesson)
+    file_path = os.path.join(module_dir, 'weiqi_conf.xml')
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    root.find('info')[lesson-1].get('code')
 
 def weiqicmd(request):
     body = request.body
     print(body)
-    cmd = struct.unpack(">h",body[:2])[0]
-    content = body[2:]
+    ba = ByteArray(_bytes)
+    cmd = ba.readShort()
     data = ""
     if cmd == 10:
-        data = _cmd10(content)
+        data = _cmd_10(ba)
+    elif cmd == 201:
+        data = _cmd_201(ba)
     resp = HttpResponse()
     resp.write(data)
     return resp
