923b0715aa3c63963ddd64096388a380115abf4a
scriptkitz <scriptkitz@gmail.com>
2018-11-07 13:24:43 +0800

    import error

--- welcome/weiqi/weiqicmd_views.py
+++ welcome/weiqi/weiqicmd_views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 
-from . import database
-from .models import PageView
-
 import ByteArray
 
 # Create your views here.
