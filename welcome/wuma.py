c83c24645863ea1c6fcf1b7d8c25631256b26bea
scriptkitz <scriptkitz@gmail.com>
2020-05-15 15:56:32 +0800

    wuma

--- welcome/wuma.py
+++ welcome/wuma.py
@@ -15,19 +15,7 @@ if g_isPy3:
 else:
     import httplib
 
-def decryptResp(data):
-    while True:
-        pos = data.find(b"Wwosxbg")
-        if pos != -1:
-            data = data[:pos-6] + data[pos+18:]
-        else:
-            pos = data.find(b"WeqBoew")
-            if pos != -1:
-                data = data[:pos] + data[pos+24:]
-            else:
-                break
-    data = base64.decodestring(data)
-    return data
+
 
 class wumaStatic:
     def __init__(self):
@@ -62,47 +50,3 @@ class wumaStatic:
         fs.write(data)
         fs.close()
 
-class wumaPhp:
-    def __init__(self):
-        self._conn = httplib.HTTPConnection("176.122.140.202",7777)
-        self._headers = {
-            "User-Agent":"okhttp/3.9.1",
-            "Accept-Encoding":"gzip",
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-    def hi(self):
-        body = {
-            "is_gp": "0",
-            "time": str(time.time()).replace(".",""),
-            "token": "HnxeJnHW3k4AyqX9lBUcG74sad1zKAyKepb",
-            "ver_code": 323,
-            "chn_name": "git_v2",
-            "lang": "zh-CN",
-            "sys_ver": 22,
-            "uid": "HnxeJ",
-            "dev_id": "2018062509580352265773",
-            "pkg": "com.muma.pn",
-            "brd_mod": "Nokia N8010"
-        }
-        if g_isPy3:
-            sbody = urllib.parse.quote(base64.encodestring(json.dumps(body).encode('gbk')))
-        else:
-            sbody = urllib.quote(base64.encodestring(json.dumps(body)))
-        sbody = sbody[:10] + "9328WRewosxbg39823B98weB" + sbody[10:]
-        sbody = sbody[:70] + "323284Wwosxbg9873298ewIE" + sbody[70:]
-        sbody = "data=" + sbody
-        data = self._send("/index.php/wmapp/hi",sbody)
-        #self._save("hellotxt",data)
-        jObj = json.loads(data)
-        return jObj
-
-    def _send(self,uri,body=None):
-        self._conn.request("POST",uri,body, headers=self._headers)
-        resp = self._conn.getresponse()
-        data = resp.read()
-        return decryptResp(data)
-
-    def _save(self,name,data):
-        fs=open(name+"_%d.json"%time.time(), "wb")
-        fs.write(data)
-        fs.close()
